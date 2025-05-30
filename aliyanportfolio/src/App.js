9c0d2e370980a6f0681bbe4b9fc7b52953b0eabb
aliyan16 <aliyanm12376@gmail.com>
2025-05-30 12:36:53 +0500

    cleaning default code

--- aliyanportfolio/src/App.js
+++ aliyanportfolio/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my portfolio2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Welcome to my portfolio2
+      </p>
     </div>
   );
 }
