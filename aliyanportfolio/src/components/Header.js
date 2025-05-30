27eb99270f5cbd3584e788bf5d08407188f11c00
aliyan16 <aliyanm12376@gmail.com>
2025-05-31 01:29:53 +0500

    added header

--- aliyanportfolio/src/components/Header.js
+++ aliyanportfolio/src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header(){
     return(
-        <>
-        <p>
-            this is header
-        </p>
-        </>
+        <header>
+            <nav>
+                <h1>My portfolio</h1>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/projects'>Projects</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
