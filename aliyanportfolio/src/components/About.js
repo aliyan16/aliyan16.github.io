74aa535cb76471b3a820b06378012195a5035ca2
aliyan16 <aliyanm12376@gmail.com>
2025-05-31 01:45:42 +0500

    customizing about

--- aliyanportfolio/src/components/About.js
+++ aliyanportfolio/src/components/About.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function About(){
     return(
-        <div>
-            <p>
-                this is my about
-            </p>
+        <div className="container">
+            <div>
+                <h3>I am Muhammad Aliyan</h3>
+                <p>a Computer Engineering student at NUST-CEME with a passion for building intelligent systems and robust applications. My expertise lies in machine learning, deep learning, and full-stack development. I have a proven track record of designing and implementing innovative solutions, from autonomous vehicle prototypes using classical image processing to cross-platform mobile applications for mission tracking. I'm adept at leveraging technologies like TensorFlow, Keras, ReactJS, and Django to deliver impactful projects. I'm committed to continuous learning and am currently expanding my knowledge in MLOps and advanced deployment strategies to further streamline the development lifecycle.</p>
+            </div>
+            <div>
+                <img src="C:\AllData\Selfskills\Portfolio\aliyan16.github.io\aliyanportfolio\public\images\Aliyan.jpg" />
+            </div>
         </div>
     )
 }
