a19519382ae19c7cb4a66c8b339185a27360f043
aliyan16 <aliyanm12376@gmail.com>
2026-02-02 01:12:19 +0500

    updated resume

--- aliyanportfolio/src/components/Header.js
+++ aliyanportfolio/src/components/Header.js
@@ -8,10 +8,10 @@ function Header() {
   const [menuActive, setMenuActive] = useState(false);
 
   const handleDownload = () => {
-    const resumeUrl = '/Aliyan_Resume.pdf';
+    const resumeUrl = '/Aliyan_CV_NEW.pdf';
     const link = document.createElement('a');
     link.href = resumeUrl;
-    link.download = 'Aliyan_Resume.pdf';
+    link.download = 'Aliyan_CV_NEW.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
